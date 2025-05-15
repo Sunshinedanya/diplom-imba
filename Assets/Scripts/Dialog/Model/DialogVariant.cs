e08fa14355a65cbb36993e3d2e7eddaa15a7f3d1
_solnushko <gkate4412@gmail.com>
2025-05-16 00:57:53 +0300

    models and dialogs, scene update, dialogs hooks are left and add some more lore

--- Assets/Scripts/Dialog/Model/DialogVariant.cs
+++ Assets/Scripts/Dialog/Model/DialogVariant.cs
@@ -8,4 +8,32 @@ namespace Dialog.Model
     {
         public List<string> responses;
     }
+    
+    [Serializable]
+    public class DialogueOption
+    {
+        public string responses;
+        public string NextNodeId;
+
+        public DialogueOption(string responses, string nextId)
+        {
+            this.responses = responses;
+            NextNodeId = nextId;
+        }
+    }
+    
+    [Serializable]
+    public class DialogueNode
+    {
+        public string CharacterId;
+        public string Response;
+        public List<DialogueOption> Options;
+
+        public DialogueNode(string response, string characterId, List<DialogueOption> options = null)
+        {
+            Response = response;
+            CharacterId = characterId;
+            Options = options ?? new List<DialogueOption>();
+        }
+    }
 }
---
+++ Assets/Scripts/Dialog/System/SimpleDialogManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Dialog.Model;
+using Interactable;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dialog.System
+{
+    public class SimpleDialogueManager : MonoBehaviour, IInteractable
+    {
+        public GameObject optionButtonPrefab;
+        public Transform optionsContainer;
+
+        private Dictionary<string, DialogueNode> dialogue = new Dictionary<string, DialogueNode>();
+    
+        private void Start()
+        {
+            SetupDialogue();
+        }
+
+        private void SetupDialogue()
+        {
+            dialogue["start"] = new DialogueNode("Привет! Как дела?", "1", new List<DialogueOption>
+            {
+                new DialogueOption("Всё хорошо.", "good"),
+                new DialogueOption("Так себе.", "bad")
+            });
+
+            dialogue["good"] = new DialogueNode("Рад слышать!", "1");
+            dialogue["bad"] = new DialogueNode("Печально. Надеюсь, всё наладится.", "1");
+        }
+
+        private void ShowNode(string nodeId)
+        {
+            if (dialogue.TryGetValue(nodeId, out var node) == false) return;
+
+            DialogueController.instance.NewDialogueInstance(node.Response, node.CharacterId);
+            ClearOptions();
+
+            foreach (var option in node.Options)
+            {
+                GameObject btn = Instantiate(optionButtonPrefab, optionsContainer);
+                btn.GetComponentInChildren<Text>().text = option.responses;
+                btn.GetComponent<Button>().onClick.AddListener(() => ShowNode(option.NextNodeId));
+            }
+        }
+
+        private void ClearOptions()
+        {
+            foreach (Transform child in optionsContainer)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        public void Interact()
+        {
+            ShowNode("start");
+        }
+
+        public string GetDescription()
+        {
+            return "Поговорить";
+        }
+    }
+
+}
